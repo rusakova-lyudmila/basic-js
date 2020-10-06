fac9001f437b88ab2b92851c32324239e02593b1
Leonid Rusakov <bengunn87@mail.ru>
2020-10-06 22:03:39 +0500

    fix: refactor dream team function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,23 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
-  /*if (members && members.length > 0) {
-    let namesFirstLetters = members.reduce((dreamTeam, item) => {
-      if (typeof item === 'string') {
-        dreamTeam += item.trim().slice(0, 1).toUpperCase();
-      }
-      return dreamTeam;
-    }, '');
-    return namesFirstLetters.split('').sort().join('');
-  } else {
-    return false;
-  }*/
-
   if (members && members.length > 0) {
     return members.filter(item => typeof item === 'string')
       .map(item => item.trim().slice(0, 1).toUpperCase())
       .sort().join('');
-  } else {
-    return false;
   }
+  
+  return false;
 };
