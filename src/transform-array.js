ab1228cef5ad243c2180c5e7213394ce4cad1164
Leonid Rusakov <bengunn87@mail.ru>
2020-10-28 18:53:05 +0500

    feat: complete transform array function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,27 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
+module.exports = function transform(arr) {
+  if (Array.isArray(arr)) {
+    const action = ['--discard-prev', '--discard-next', '--double-next', '--double-prev'];    
+    return arr.reduce((res, item, ind) => {
+      if (action.includes(item) || arr[ind - 1] === '--discard-next') {
+        return res;
+      }
+      if (arr[ind - 1] === '--double-next' && arr[ind + 1] === '--discard-prev') {
+        return [...res, item];
+      }
+      if (arr[ind - 1] === '--double-next' && arr[ind + 1] === '--double-prev') {        
+        return [...res, item, item, item];
+      }
+      if (arr[ind - 1] === '--double-next' || arr[ind + 1] === '--double-prev') {        
+        return [...res, item, item];
+      }    
+      if (arr[ind + 1] === '--discard-prev') {
+        return res;                
+      }      
+      return [...res, item];        
+    }, []); 
+  }
+
   throw new CustomError('Not implemented');
-  // remove line with error and write your code here
 };
